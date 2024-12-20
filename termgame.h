cffaaf74b5e63f38bc02cf3746c7d1509fe3fa32
DMGDy <dylangarza1909@gmail.com>
2024-12-20 09:09:03 +0700

    [termgame.h] created

---
+++ termgame.h
@@ -0,0 +1,32 @@
+#ifndef TERMGAME_H_
+#define TERMGAME_H_
+/*
+   termgame.h
+   Copyright (C) ${license.years} ${license.owner}
+   Copyright (C) 2024 Dylan Dy or Dylan-Matthew Garza
+   <dylangarza1909@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// some stuff
+
+#endif // TERMGAME_H_
+
+#ifndef TERMGAME_IMPLEMENTATION
+#define TERMGAME_IMPLEMENTATION
+
+//some stuff
+
+#endif
